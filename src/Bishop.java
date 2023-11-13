fee792fbcb738b1ea61237451aba7ce93654c4de
Bensvn <bensvn1981@gmail.com>
2023-11-13 20:35:59 +0500

    проект Шахматы

---
+++ src/Bishop.java
@@ -0,0 +1,65 @@
+import static java.lang.Math.*;
+public class Bishop extends ChessPiece{
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine!=line && toColumn!=column)
+        {
+            if (toLine >= 0 && toLine < 8 && toColumn >= 0 && toColumn < 8) {
+                if ((((toLine -line) == (toColumn -column ))
+                        ||
+                        ((toLine -line) == -1*(toColumn -column )))
+                        &&isClearDiagonal(chessBoard,line,column,toLine,toColumn)
+                )
+                {
+                    return true;}
+                else {
+                    return false;}
+
+            }else {
+                return false;}
+        }
+        else {
+            return false;}
+
+    }
+
+    private boolean isClearDiagonal(ChessBoard chessBoard,  int line, int column, int toLine, int toColumn) {
+        int n=0;
+        int m=0;
+        boolean isClear = true;
+        for (int i = 1;i<=Math.abs(toLine-line);i=i+1){
+            if(toLine>line)
+            {n=line+i;
+                if(toColumn>column)
+                {m=column+i;}else {m=column-i;}
+            }else{
+                n=line-i;
+                if(toColumn>column)
+                {m=column+i;}else {m=column-i;}}
+            if (chessBoard.board[n][m]==null)
+            {isClear=true;}
+            else
+            {
+                if (n==toLine && m==toColumn
+                        && chessBoard.board[toLine][toColumn].getColor()!=chessBoard.board[line][column].getColor())
+                {isClear=true;} else {isClear=false;return isClear;}
+            }
+
+        }
+        return isClear;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "B";
+    }
+}
